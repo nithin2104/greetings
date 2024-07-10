3d274cee60f5c2dcd56226520b451038d21b8930
nithin kumar <nithin54k@gmail.com>
2024-07-10 11:49:07 +0530

    CREATED GREETINGS LOCAL PLUGIN and displayed greetings message

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Folder module version information
+ *
+ * @package   local_greetings
+ * @copyright 2024 Nithin Kumar nithin54k@gmail.com
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../config.php');
+$context = CONTEXT_SYSTEM::instance();
+$PAGE->set_context($context);
+$PAGE->set_url(new moodle_url("/local/greetings/index.php"));
+$PAGE->set_pagelayout("standard");
+$PAGE->set_title(get_string('pluginname', 'local_greetings'));
+$PAGE->set_heading(get_string('pluginname', 'local_greetings'));
+
+
+echo $OUTPUT->header();
+if (isloggedin()) {
+    echo "<h3>Greetings, " . fullname($USER) . "</h3>";
+} else {
+    echo "<h3>Greetings, USER</h3>";
+}
+
+
+echo $OUTPUT->footer();
+
